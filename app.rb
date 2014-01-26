1cf7479164c69ec7e1377bf21fa556dbe6133636
Naoto Enokawa <enokawa00@gmail.com>
2014-01-26 21:43:38 +0900

    test

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'twitter'
 require 'sinatra'
+require 'uri'
+require 'net/http'
 
 TW_CONSUMER_KEY        = "bJAb7SxAbRLiUSz71OMsg"
 TW_CONSUMER_SECRET     = "DEj6kayV1V7AD8jXRAvs3BfNg52ofCjovCr2K9o4"
@@ -19,14 +21,19 @@ get '/' do
 word = "#macfriends";
 puts results = twClient.search(word, :count => 100, :result_type => "recent")
 
+
 html = ""
    results.attrs[:statuses].each do |tweet|
      #html = tweet[:user].to_s
-     #html += "<p>"+tweet[:user].to_s+"</p>"
-     html += "<h1>"+"@"+tweet[:user][:screen_name]+"</h1>"
-     html += "<h2>"+tweet[:text].to_s+"</h2>"
+     html += "<p>"+tweet.to_s+"</p>"
+     html += "<h1>"+tweet[:user][:name]+"@"+tweet[:user][:screen_name]+"</h1>"
+     html += "<h2>"+tweet[:entities][:media].to_s+"</h2>"
+     a = tweet[:entities][:media]
+     html += "<p>"+tweet.to_s+"</p>"
+     #html += "<p>"+a[:media_url]+"</p>"
+     #html += "<p>"+a.[:media_url].to_s"</p>"
      #html += "<img src="+tweet[:user][:url].to_s+">"
-     #html += "<p>"+tweet[:user].to_s+"</p>"
+     #html += "<p>"+tweet[:user][:entities][:media][:media_url]+"</p>"
      #html += "<br>"
    end
    html
