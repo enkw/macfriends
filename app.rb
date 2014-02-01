33fb8a4dac01d11e55b6135e90b7eb74fe97dddc
Naoto Enokawa <enokawa00@gmail.com>
2014-02-01 21:34:47 +0900

    change the layout

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 require 'twitter'
 require 'sinatra'
-require 'uri'
-require 'net/http'
 
 TW_CONSUMER_KEY        = "bJAb7SxAbRLiUSz71OMsg"
 TW_CONSUMER_SECRET     = "DEj6kayV1V7AD8jXRAvs3BfNg52ofCjovCr2K9o4"
 TW_ACCESS_TOKEN        = "381595197-K6H2cXUSPAXBiqZd67i206pMFgpe9UevVPD1ty62"
 TW_ACCESS_TOKEN_SECRET = "TA9ocbEWpjoIG9Om35zc9or2RVTPysbad0P3NmkAA02ai"
+
+#@Client = Twitter::REST::Client
+#twClient = @Client.new do |config|
+#	config.consumer_key        = TW_CONSUMER_KEY
+#    config.consumer_secret     = TW_CONSUMER_SECRET
+#    config.access_token        = TW_ACCESS_TOKEN
+#    config.access_token_secret = TW_ACCESS_TOKEN_SECRET
+#end
 
 twClient = Twitter::REST::Client.new do |config|
     config.consumer_key        = TW_CONSUMER_KEY
@@ -18,27 +24,27 @@ end
 
 get '/' do
 
-word = "#macfriends";
-puts results = twClient.search(word, :count => 100, :result_type => "recent")
+	word = "#macfriends";
+	puts results = twClient.search(word, :count => 100, :result_type => "recent")
+	#html = ""
+	results.attrs[:statuses].each do |tweet|
+		#html += "<h2>"+"@"+tweet[:user][:screen_name]+"</h2>"
+		#html += "<p>"+tweet[:text].to_s+"</p>"
+    	#if tweet[:entities][:media] != nil
+		#html += "<img src="+tweet[:entities][:media][0][:media_url].to_s+">"
+		#end
+		#html += "<p>"+tweet.to_s+"</p>"
 
-
-html = ""
-   results.attrs[:statuses].each do |tweet|
-     #html = tweet[:user].to_s
-     html += "<h2>"+"@"+tweet[:user][:screen_name]+"</h2>"
-     html += "<p>"+tweet[:text].to_s+"</p>"
-     #html += "<p>"+tweet[:entities].to_s+"</p>"
-     #html += "<p>"+a[:media_url]+"</p>"
-     #html += "<p>"+a.[:media_url].to_s"</p>"
-     #html += "<img src="+tweet[:user][:url].to_s+">"
-     #html += "<p>"+tweet[:user][:entities][:media][:media_url]+"</p>"
-     #html += "<br>"
-   end
-   html
+		@username = tweet[:user][:screen_name].to_s
+		@text = tweet[:text].to_s
+		if tweet[:entities][:media] != nil
+			@media_url = tweet[:entities][:media][0][:media_url].to_s
+		end
+	end
+   	#html
+	erb :index
 end
 
-get '/tweet/' do
-
-  @hello = "こんにちは！じぇじぇじぇ！"
-  erb :index
+get '/about' do
+  erb :about
 end
