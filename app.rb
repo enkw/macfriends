0bfa32b08d8c206329485141c19e029162bfed0e
Naoto Enokawa <enokawa00@gmail.com>
2014-02-08 16:59:12 +0900

    hide twitter application key

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'twitter'
 require 'sinatra'
 
-TW_CONSUMER_KEY        = "your_twitter_consumer_key"
-TW_CONSUMER_SECRET     = "your_twitter_consumer_secret"
-TW_ACCESS_TOKEN        = "your_twitter_access_token"
-TW_ACCESS_TOKEN_SECRET = "your_twitter_access_token_secret"
+TW_CONSUMER_KEY        = ENV["TWCK"]
+TW_CONSUMER_SECRET     = ENV["TWCS"]
+TW_ACCESS_TOKEN        = ENV["TWAT"]
+TW_ACCESS_TOKEN_SECRET = ENV["TWATS"]
 
 twClient = Twitter::REST::Client.new do |config|
     config.consumer_key        = TW_CONSUMER_KEY
