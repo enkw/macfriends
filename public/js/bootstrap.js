930be7ef53f3b8fa057e84d4950d6f8d3475e669
enokawa <enokawa00@gmail.com>
2015-02-10 02:19:38 +0900

    上にいくボタンを設置

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -2003,4 +2003,24 @@ if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery"
     })
   })
 
+  $(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
 }(jQuery);
